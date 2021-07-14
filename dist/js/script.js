01ecdd9c12b4dd4062295d90986eb8d36e77a592
Kraevskiy Mikhail <mixail.jazz@yandex.ru>
2021-07-14 20:22:17 +0800

    create 4th section and fix some issues

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,9 @@ const hamburger = document.querySelector(".hamburger"),
     menu = document.querySelector(".menu"),
     menuClose = document.querySelector(".menu__close"),
     overlay = document.querySelector(".menu__overlay"),
-    menuLink = document.querySelectorAll(".menu__link");
+    menuLinks = document.querySelectorAll(".menu__link"),
+    skillsCounters = document.querySelectorAll(".using__skills-percent"),
+    slillsBars = document.querySelectorAll(".bar__fill");
 
 hamburger.addEventListener("click", () => {
     menu.classList.add("active");
@@ -19,9 +21,13 @@ overlay.addEventListener("click", () => {
     document.body.style.overflow = "visible";
 });
 
-menuLink.forEach((item) => {
+menuLinks.forEach((item) => {
     item.addEventListener("click", () => {
         menu.classList.remove("active");
         document.body.style.overflow = "visible";
     });
+});
+
+skillsCounters.forEach((item, i) => {
+    slillsBars[i].style.width = item.innerHTML;
 });
