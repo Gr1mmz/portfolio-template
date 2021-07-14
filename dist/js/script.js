d95dcecce5273f80ed00f4d12f506885e93f9375
Kraevskiy Mikhail <mixail.jazz@yandex.ru>
2021-07-14 12:38:27 +0800

    add second section and some animations

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,18 +6,22 @@ const hamburger = document.querySelector(".hamburger"),
 
 hamburger.addEventListener("click", () => {
     menu.classList.add("active");
+    document.body.style.overflow = "hidden";
 });
 
 menuClose.addEventListener("click", () => {
     menu.classList.remove("active");
+    document.body.style.overflow = "visible";
 });
 
 overlay.addEventListener("click", () => {
     menu.classList.remove("active");
+    document.body.style.overflow = "visible";
 });
 
 menuLink.forEach((item) => {
     item.addEventListener("click", () => {
         menu.classList.remove("active");
+        document.body.style.overflow = "visible";
     });
 });
